12ac6b56d35f15a8a9fed7b3ac818b454d1b3386
Gary J <github@accu.ca>
2021-09-03 15:39:57 -0700

    Add soclet.io Client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import 'App.css';
+import { useEffect, useState } from 'react';
+import io from "socket.io-client";
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const socket = io("/");
+    socket.on('connect', event => {
+      console.log("connected");
+    });
+
+    socket.on('public', msg => {
+      setMessages([msg, ...messages]);
+    });
+
+    // ensures we disconnect to avoid memory leaks
+    return () => socket.disconnect();
+  }, []);
+
+  const list = messages.map((msg, i) => <li key={i}>{msg}</li>);
 
   return (
     <div className="App">
       <h1>React App</h1>
+
+      <ul>
+        {list}
+      </ul>
+
     </div>
   );
 }
